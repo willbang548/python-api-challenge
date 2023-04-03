64849bea83caadcba3a447ace357127cd9665da5
willbang548 <zw2837@tc.columbia.edu>
2023-04-03 09:53:39 -0400

    Homework final

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8b95bae9e3b17afd8c63d361d0cb5575"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d14cf0c00f0b4885ad0e3ebbf89d57c7"
